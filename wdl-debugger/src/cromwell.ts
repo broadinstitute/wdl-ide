961ffc19070668cd02656303e468398ae387ad29
Denis Loginov <denis@broadinstitute.org>
2019-05-27 13:09:00 -0400

    Watch for workflow completion/failure/cancellation in wdl-debugger

--- wdl-debugger/src/cromwell.ts
+++ wdl-debugger/src/cromwell.ts
@@ -1,12 +1,17 @@
 import { createReadStream } from 'fs';
-import { post } from 'request-promise-native';
+import { get, post } from 'request-promise-native';
 
-export const submitWorkflow = (
+export type WorkflowResponse = {
+  id: string;
+  status: string;
+};
+
+export const postWorkflow = async (
   baseUri: string,
   wdlPath: string,
   inputsPath: string,
   optionsPath: string,
-) => {
+): Promise<WorkflowResponse> => {
   return post(baseUri, {
     json: true,
     formData: {
@@ -14,5 +19,23 @@ export const submitWorkflow = (
       workflowInputs: createReadStream(inputsPath),
       workflowOptions: createReadStream(optionsPath),
     },
+  });
+};
+
+export const getWorklowStatus = async (
+  baseUri: string,
+  id: string,
+): Promise<WorkflowResponse> => {
+  return get(`${baseUri}/${id}/status`, {
+    json: true,
+  });
+};
+
+export const abortWorkflow = async (
+  baseUri: string,
+  id: string,
+): Promise<WorkflowResponse> => {
+  return post(`${baseUri}/${id}/abort`, {
+    json: true,
   });
 };
--- wdl-debugger/src/extension.ts
+++ wdl-debugger/src/extension.ts
@@ -1,17 +1,24 @@
 import { promises as fs } from 'fs';
 import * as path from 'path';
-
 import {
 	ExtensionContext,
+	ProgressLocation,
+	Uri,
 	commands,
 	window,
 	workspace,
 } from 'vscode';
 
-import { submitWorkflow } from './cromwell';
+import {
+	WorkflowResponse,
+	abortWorkflow,
+	getWorklowStatus,
+	postWorkflow,
+} from './cromwell';
 
 const cmdRunWDL = 'extension.runWDL';
 const cromwellBaseUriConfig = 'wdl-debugger.cromwell.baseUri';
+const cromwellPollMsecConfig = 'wdl-debugger.cromwell.pollMsec';
 
 export const activate = async (context: ExtensionContext) => {
 	const storagePath = context.storagePath!;
@@ -32,24 +39,24 @@ export const activate = async (context: ExtensionContext) => {
 				throw Error('This command is only valid for WDL files!');
 			}
 
-			const resourceConfig = workspace.getConfiguration('', uri);
-			let cromwellBaseUri = resourceConfig.get<string>(cromwellBaseUriConfig)!;
-
+			const cromwellBaseUri = getConfig<string>(uri, cromwellBaseUriConfig);
+			const cromwellPollMsec = getConfig<number>(uri, cromwellPollMsecConfig);
 			const wdlPath = uri.fsPath;
-			const { id, status } = await submitWorkflow(
-				cromwellBaseUri,
-				wdlPath,
-				await getInputsPath(storagePath, wdlPath),
-				await getOptionsPath(storagePath),
-			);
 
-			window.showInformationMessage(`${status} ${id}`);
+			await runAndWait(
+				cromwellBaseUri, cromwellPollMsec, storagePath, wdlPath,
+			);
 		} catch (err) {
-			window.showErrorMessage(err.message);
+			showError(err);
 		}
 	});
 
 	context.subscriptions.push(disposable);
+};
+
+const getConfig = <T>(uri: Uri | undefined, section: string) => {
+	const config = workspace.getConfiguration('', uri);
+	return config.get<T>(section)!;
 };
 
 const getInputsPath = (storagePath: string, wdlPath: string) => {
@@ -71,6 +78,71 @@ const getFilePath = async (storagePath: string, fileName: string) => {
 		}
 	}
 	return filePath;
+};
+
+const runAndWait = async (
+	cromwellBaseUri: string,
+	cromwellPollMsec: number,
+	storagePath: string,
+	wdlPath: string,
+) => {
+	const { id, status } = await postWorkflow(
+		cromwellBaseUri,
+		wdlPath,
+		await getInputsPath(storagePath, wdlPath),
+		await getOptionsPath(storagePath),
+	);
+
+	return window.withProgress({
+		location: ProgressLocation.Notification,
+		title: `Workflow ${id}`,
+		cancellable: true,
+	}, async (progress, token) => {
+		return new Promise<WorkflowResponse>(resolve => {
+			const interval = setInterval(async () => {
+				try {
+					const { status } = await getWorklowStatus(cromwellBaseUri, id);
+					checkStopPolling(status);
+				} catch (err) {
+					showError(err);
+				}
+			}, cromwellPollMsec);
+
+			let oldStatus = '';
+			const checkStopPolling = (status: string) => {
+				const message = `Workflow ${id}: ${status}`;
+				switch (status) {
+					case 'Succeeded':
+						window.showInformationMessage(message); break;
+					case 'Aborting': case 'Aborted':
+						window.showWarningMessage(message); break;
+					case 'Failed':
+						window.showErrorMessage(message); break;
+					default:
+						if (status !== oldStatus) {
+							progress.report({
+								message: status,
+							});
+						}
+						oldStatus = status;
+						return;
+				}
+				clearInterval(interval);
+				resolve({ id, status });
+			};
+
+			checkStopPolling(status);
+
+			token.onCancellationRequested(async () => {
+				const { status } = await abortWorkflow(cromwellBaseUri, id);
+				checkStopPolling(status);
+			});
+		});
+	});
+};
+
+const showError = (err: Error) => {
+	return window.showErrorMessage(err.message);
 };
 
 export function deactivate() {}
