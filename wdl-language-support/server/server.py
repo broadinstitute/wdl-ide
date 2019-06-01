361da0bb0d10bee3cf064eac8c0792aa5c763e92
Denis Loginov <denis@broadinstitute.org>
2019-06-01 15:23:10 -0400

    Add validation errors to LS

--- wdl-language-support/server/server.py
+++ wdl-language-support/server/server.py
@@ -36,31 +36,41 @@ def _validate(ls, params):
 
 def _validate_wdl(ls, uri):
     """Validates WDL file."""
-    diagnostics = []
-
     try:
         a = WDL.load(uri)
         ls.show_message_log('Validated')
+
     except WDL.Error.SyntaxError as e:
         err = e.args[0]
-        match = re.match(r"\(.*\) (.*) at line (\d+) col (\d+)", err)
+        match = re.match("^\(.*\) (.*) at line (\d+) col (\d+)", err)
         msg = match.group(1)
         line = int(match.group(2))
         col = int(match.group(3))
+        return [_get_diagnostic(msg, line, col, line, sys.maxsize)]
 
-        d = Diagnostic(
-            Range(
-                Position(line-1, col-1),
-                Position(line-1, sys.maxsize)
-            ),
-            msg,
-            source=type(server).__name__
-        )
+    except WDL.Error.ValidationError as e:
+        return [_add_validaton_error(e)]
 
-        diagnostics.append(d)
+    except WDL.Error.MultipleValidationErrors as errs:
+        return [_add_validaton_error(e) for e in errs.exceptions]
 
-    return diagnostics
+    return []
 
+def _get_diagnostic(msg, line, col, end_line, end_col):
+    return Diagnostic(
+        Range(
+            Position(line - 1, col - 1),
+            Position(end_line - 1, end_col - 1)
+        ),
+        msg,
+        source=type(server).__name__
+    )
+
+def _add_validaton_error(e: WDL.Error.ValidationError):
+    err = e.args[0]
+    msg = re.match("^\(.*\) (.*)", err).group(1)
+    pos = e.pos
+    return _get_diagnostic(msg, pos.line, pos.column, pos.end_line, pos.end_column)
 
 @server.feature(TEXT_DOCUMENT_DID_OPEN)
 async def did_open(ls, params: DidOpenTextDocumentParams):
