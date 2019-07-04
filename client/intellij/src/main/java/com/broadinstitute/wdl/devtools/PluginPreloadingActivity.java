694c83a1760f5a25e6c7ab1c4e570c0df3d89131
Denis Loginov <denis@broadinstitute.org>
2019-07-04 10:20:00 -0400

    Initial support for Syntax Highlighting and LSP for IntelliJ plugin
    This commit adds org.jetbrains.plugins.textmate
    and lsp4intellij library, and configures them minimally to:
    
    - display syntax highlighting using WDL.tmLanguage definitions
      from broadinstitute/wdl-sublime-syntax-highlighter repo
    
    - start the language server (if present)
    
    While syntax highlighting seems to work well,
    the LSP is not functional yet because while we
    see the server logs with successful messages,
    the IntelliJ client doesn't display any diagnostics
    reported by the server yet.

---
+++ client/intellij/src/main/java/com/broadinstitute/wdl/devtools/PluginPreloadingActivity.java
@@ -0,0 +1,56 @@
+package com.broadinstitute.wdl.devtools;
+
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.ide.plugins.PluginManagerMain;
+import com.intellij.openapi.application.PluginPathManager;
+import com.intellij.openapi.application.PreloadingActivity;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.progress.ProgressIndicator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.textmate.TextMateService;
+import org.jetbrains.plugins.textmate.configuration.BundleConfigBean;
+import org.jetbrains.plugins.textmate.configuration.TextMateSettings;
+import org.wso2.lsp4intellij.IntellijLanguageClient;
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.ExeLanguageServerDefinition;
+
+import java.io.File;
+import java.util.Collections;
+
+public final class PluginPreloadingActivity extends PreloadingActivity {
+
+    private static final String PLUGIN_ID = "org.broadinstitute.wdl.devtools";
+
+    private static final String BUNDLE = "WDL";
+
+    private static final String EXTENSION = "wdl";
+    private static final String PYTHON_PATH = "python3";
+    private static final String SERVER_MODULE = "wdl_lsp";
+
+    @Override
+    public void preload(@NotNull ProgressIndicator indicator) {
+        setupSyntaxHighlighting();
+
+        setupLanguageServer();
+    }
+
+    private void setupSyntaxHighlighting() {
+        TextMateSettings.TextMateSettingsState state = TextMateSettings.getInstance().getState();
+        if (state == null) {
+            state = new TextMateSettings.TextMateSettingsState();
+        }
+
+        final File pluginDir = PluginManager.getPlugin(PluginId.getId(PLUGIN_ID)).getPath();
+        final String bundlePath = new File(pluginDir, "WDL.tmbundle").getAbsolutePath();
+        final BundleConfigBean bundle = new BundleConfigBean(BUNDLE, bundlePath, true);
+
+        state.setBundles(Collections.singleton(bundle));
+        TextMateSettings.getInstance().loadState(state);
+        TextMateService.getInstance().registerEnabledBundles(false);
+    }
+
+    private void setupLanguageServer() {
+        IntellijLanguageClient.addServerDefinition(
+                new ExeLanguageServerDefinition(EXTENSION, PYTHON_PATH, new String[]{"-m", SERVER_MODULE})
+        );
+    }
+}
