7f29ace6c5f53f5ce6ac482dde201decc08c1019
Denis Loginov <denis@broadinstitute.org>
2019-07-08 13:28:50 -0400

    Set path to Python3 executable through wdl.pythonPath global property in IntelliJ

--- client/intellij/src/main/java/org/broadinstitute/wdl/devtools/PluginPreloadingActivity.java
+++ client/intellij/src/main/java/org/broadinstitute/wdl/devtools/PluginPreloadingActivity.java
@@ -2,6 +2,7 @@ package org.broadinstitute.wdl.devtools;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.PluginId;
@@ -28,7 +29,8 @@ public final class PluginPreloadingActivity extends PreloadingActivity {
     private static final String BUNDLE = "WDL";
 
     private static final String EXTENSION = "wdl";
-    private static final String PYTHON_PATH = "python3";
+    private static final String PYTHON_PATH_PROPERTY = "wdl.pythonPath";
+    private static final String PYTHON_PATH_DEFAULT = "/usr/local/bin/python3";
     private static final String SERVER_MODULE = "wdl_lsp";
 
     @Override
@@ -55,13 +57,15 @@ public final class PluginPreloadingActivity extends PreloadingActivity {
     }
 
     private void setupLanguageServer(final IdeaPluginDescriptor plugin) {
+        final PropertiesComponent settings = PropertiesComponent.getInstance();
+        final String pythonPath = settings.getValue(PYTHON_PATH_PROPERTY, PYTHON_PATH_DEFAULT);
         final String version = plugin.getVersion();
         final boolean isRelease = version.matches(RELEASE_REGEX);
-        if (isRelease && !runProcess(PYTHON_PATH, "-m", "pip", "install", "--user", "wdl-lsp==" + version)) {
+        if (isRelease && !runProcess(pythonPath, "-m", "pip", "install", "--user", "wdl-lsp==" + version)) {
             return;
         }
         IntellijLanguageClient.addServerDefinition(
-                new ExeLanguageServerDefinition(EXTENSION, PYTHON_PATH, new String[]{
+                new ExeLanguageServerDefinition(EXTENSION, pythonPath, new String[]{
                         "-m", SERVER_MODULE, "--log", isRelease ? "WARNING" : "DEBUG",
                 })
         );
