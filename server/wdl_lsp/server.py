60d4347ecc4b07fc5d60b25296264582b56b03df
Denis Loginov <denis@broadinstitute.org>
2019-08-12 15:14:59 -0400

    [server] Fix resolved URI prefix in read_source()

--- server/wdl_lsp/server.py
+++ server/wdl_lsp/server.py
@@ -133,6 +133,8 @@ def _parse_wdl(ls: Server, uri: str):
 def _read_source(ls: Server):
     async def read_source(uri: str, path, importer):
         uri = await WDL.Tree.resolve_file_import(uri, path, importer)
+        if uri.startswith('/'):
+            uri = 'file://' + uri
         source = ls.workspace.get_document(uri).source
         return WDL.ReadSourceResult(source_text=source, abspath=uri)
     return read_source
