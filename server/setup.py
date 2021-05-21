c00d5bb627c02f9087d1de6ceebc25c46b5db3af
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-05-21 12:32:14 +1000

    Upgrade MiniWDL version

--- server/setup.py
+++ server/setup.py
@@ -31,7 +31,7 @@ setup(
     },
     install_requires=[
         'cromwell-tools == 2.3.0',
-        'miniwdl == 0.5.1',
+        "miniwdl >= 1.1.5",
         'pygls == 0.8.1',
     ],
     entry_points={
