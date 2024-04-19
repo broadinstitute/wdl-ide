bbbe02af20b13e2b7f955e3a6dd781d537c7e41d
Denis Loginov <137337+dinvlad@users.noreply.github.com>
2024-04-19 16:39:17 -0400

    GitHub workflows (#33)
    * GitHub workflows
    
    * Add PyPI workflow path
    
    * Fix package dir
    
    * Remove setuptools_scm
    
    * Fix wheel build
    
    * Fix ref
    
    * Add VSCode workflow
    
    * Rename VS Code workflow
    
    * Remove Node version
    
    * Update package-lock
    
    * Add JetBrains workflow
    
    * Fix working dir for pypi
    
    * Fix workdir for vscode
    
    * Fix Python step name

--- server/setup.py
+++ server/setup.py
@@ -22,9 +22,6 @@ setup(
             'tests',
         ],
     ),
-    setup_requires=[
-        'setuptools_scm',
-    ],
     use_scm_version={
         'root': '..',
         'relative_to': __file__,
